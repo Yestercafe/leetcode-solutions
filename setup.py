eaf0c4c098e89825917371fb8783c200db198f00
Ivan Chien <qyc027@gmail.com>
2021-03-05 17:06:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 from components.migrate import migrate
 from components.enrich_id import enrich_id
 from components.git import git
+import os
+
+def cmd(command):
+  print(command)
+  os.system(command)
 
 migrate('Yescafe', '.', './leetcode-everyday')
 enrich_id('.', 4)
+cmd('git add ./leetcode-everyday')
 git('addp')
 git('commit')
 git('sync')
