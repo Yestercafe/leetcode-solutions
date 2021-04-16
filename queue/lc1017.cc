39850bc1f07e424b2a9f5c0eccfa889cf8439ede
Ivan Chien <qyc027@gmail.com>
2021-04-16 10:35:12 +0800

    Update todo.org

---
+++ queue/lc1017.cc
@@ -0,0 +1,20 @@
+#include <string>
+using std::string;
+
+class Solution {
+public:
+  string baseNeg2(int n) {
+
+    return {};
+  }
+};
+
+#include <iostream>
+
+int main()
+{
+  int n = 10;
+  std::cout << Solution().baseNeg2(n) << std::endl;
+
+  return 0;
+}
