c68707c63d93aaca6806720856b33da174684ed2
Ivan Chien <qyc027@gmail.com>
2021-11-12 13:59:42 +0800

    Optimize pick.py

--- components/pick.py
+++ components/pick.py
@@ -2,6 +2,8 @@
 import json
 import os
 
+LC_PREFIX = 'https://leetcode.com/problems/'
+
 def get_problem_name(pick_id):
     with open('components/problems.json') as fp:
         for info in json.load(fp)['stat_status_pairs']:
@@ -18,7 +20,8 @@ def mk_problem_dir(pick_id, problem_name):
     except FileExistsError:
         return False
     with open(dir_name + '/readme.md', 'w+') as w:
-        w.write('# %d. %s' % (pick_id, problem_name['title']))
+        w.write('# %d. %s\n' % (pick_id, problem_name['title']))
+        w.write(LC_PREFIX + problem_name['slug'] + '\n')
     return True
 
 def pick_problem(pick_id):
@@ -26,7 +29,7 @@ def pick_problem(pick_id):
     if not mk_problem_dir(pick_id, problem_name):
         print('Dir existed!')
     else:
-        print('Add %d succeeded!' % pick_id)
+        print('Add %d-%s succeeded!' % (pick_id, problem_name['title']))
 
 def main():
     pick_id = int(input('ID you pick: '))
