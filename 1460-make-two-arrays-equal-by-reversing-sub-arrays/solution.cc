7e4fc6991d82e551f8560e0c464e1a7720d54c01
Ivan Chien <qyc027@gmail.com>
2022-08-24 10:50:41 +0800

    Add 1460

---
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/solution.cc
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        return equal(target.begin(), target.end(), arr.begin());
+    }
+};
+
