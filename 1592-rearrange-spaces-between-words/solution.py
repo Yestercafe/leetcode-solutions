101aa2fb2cf5c062df247b015ad3b1bca0c2fdea
Ivan Chien <qyc027@gmail.com>
2022-09-07 18:32:02 +0800

    Update 1592 Python solution

--- 1592-rearrange-spaces-between-words/solution.py
+++ 1592-rearrange-spaces-between-words/solution.py
@@ -2,8 +2,6 @@ class Solution:
     def reorderSpaces(self, text: str) -> str:
         words = text.split()
         cnt_spc = len(text) - len(''.join(words))
-        if cnt_spc == 0:
-            return text
         if len(words) < 2:
             return words[0] + ' ' * cnt_spc
         quo, rem = cnt_spc // (len(words) - 1), cnt_spc % (len(words) - 1)
