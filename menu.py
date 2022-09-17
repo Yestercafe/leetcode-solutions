461d8429f8964fd79838131fd95dff09e00c07c2
Ivan Chien <qyc027@gmail.com>
2022-09-17 12:43:32 +0800

    update: updated scripts

--- menu.py
+++ menu.py
@@ -11,28 +11,35 @@ import scripts.utils as utils
 
 def main():
     parser = argparse.ArgumentParser(description='An automation script for this repo.')
-    parser.add_argument('instruction', type=str, choices=['migrate', 'pick', 'info', 'update_readme', 'update_problem_list', 'auto_commit', 'stats'])
+    parser.add_argument('instruction', type=str, choices=['migrate', 'pick', 'info', 'update_readme', 'update_problem_list', 'update_scripts', 'auto_commit', 'stats'])
     parser.add_argument('-i', '--problem_id', type=int)
     args = parser.parse_args()
-
+    
+    commit_info = ''
     if args.instruction == 'migrate':
         migrate.migrate_from_repo('leetcode-everyday', 'Yescafe')
+        commit_info = 'migrate: ran migrate script'
     elif args.instruction == 'pick':
         Problem.pick_problem(args.problem_id)
+        commit_info = 'pick: picked {}'.format(args.problem_id)
     elif args.instruction == 'info':
         print(Problem(args.problem_id, PJson().export))
         return
     elif args.instruction == 'update_readme':
         migrate.update_readmes()
+        commit_info = 'update: updated readme files'
     elif args.instruction == 'update_problem_list':
         update_problem_list()
+        commit_info = 'update: updated problem_list.json'
+    elif args.instruction == 'update_scripts':
+        commit_info = 'update: updated scripts'
     elif args.instruction == 'auto_commit':
         auto_commit()
         return
     elif args.instruction == 'stats':
         utils.run('ls | grep "^\d" | wc -l')
         return
-    auto_commit()
+    auto_commit(commit_info)
 
 
 
--- scripts/git.py
+++ scripts/git.py
@@ -2,9 +2,12 @@ import time
 import scripts.utils as utils
 
 
-def auto_commit():
+def auto_commit(commit_info: str = ''):
     add_all()
-    commit('Auto commit: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    if len(commit_info) == 0:
+        commit('Auto commit: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    else:
+        commit(commit_info)
 
 def commit(msg: str):
     utils.run('git commit -m "' + msg + '"')
