600e12c19ae0f9759fb3102fdb57243a068bc37b
Yescafe <qyc027@gmail.com>
2023-02-18 18:26:02 +0800

    Add 1237

---
+++ 1237-find-positive-integer-solution-for-a-given-equation/solution.cc
@@ -0,0 +1,38 @@
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> findSolution(CustomFunction& cf, int z) {
+        vector<vector<int>> ans;
+        int y = 1000;
+        for (int x = 1; x <= 1000; ++x) {
+            if (y == 0) break;
+            int left = 0, right = y + 1;
+            while (left + 1 < right) {
+                int mid = (left + right) / 2;
+                if (cf.f(x, mid) < z) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+            if (cf.f(x, right) == z) {
+                y = right;
+                ans.push_back({x, y});
+                if (y == 1) break;
+            }
+        }
+        return ans;
+    }
+};
+
