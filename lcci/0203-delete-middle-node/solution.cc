3547158213e039d571ac9e79bc11c7d7ba4dd939
Ivan Chien <qyc027@gmail.com>
2023-12-03 14:48:55 +0800

    Add lcci/0203-delete-middle-node/solution.cc

---
+++ lcci/0203-delete-middle-node/solution.cc
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        auto nxt = node->next;
+        node->val = nxt->val;
+        node->next = nxt->next;
+    }
+};
+
